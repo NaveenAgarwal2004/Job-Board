a413cdbd7dc3deb9bfb6cc8cd60221f01e97511b
NaveenAgarwal2004 <naveenagarwal7624@gmail.com>
2025-09-15 22:37:27 +0530

    Update Applications.tsx

--- src/pages/Applications.tsx
+++ src/pages/Applications.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-hot-toast';
 import { 
   FileText, Clock, CheckCircle, XCircle, User, Building, 
